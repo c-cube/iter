14d651b83622b73cf355b4b0522eb9640266d901
Simon Cruanes <simon.cruanes.2007@m4x.org>
2013-02-04 23:29:09 +0100

    S-expression module, with a strong accent on sequences (o'rly?). S-exprs can be converted to and from streams of 'tokens'

--- tests.ml
+++ tests.ml
@@ -9,6 +9,8 @@ let pp_list ?(sep=", ") pp_item formatter l =
 module ISet = Set.Make(struct type t = int let compare = compare end)
 module ISetSeq = Sequence.Set(ISet)
 
+let sexpr = "(foo bar (bazz quux hello 42) world (zoo foo bar (1 2 (3 4))))"
+
 let _ =
   let l = [0;1;2;3;4;5;6] in
   let l' = Sequence.List.of_seq
@@ -38,8 +40,10 @@ let _ =
       (Sequence.Array.of_seq (Sequence.append
         (Sequence.take 5 (Sequence.List.to_seq l3)) (Sequence.List.to_seq l4))));
   (* sum *)
-  let n = 200000000 in
+  let n = 100000000 in
   let sum = Sequence.fold (+) 0 (Sequence.take n (Sequence.Int.repeat 1)) in
   Format.printf "%dx1 = %d@." n sum;
   assert (n=sum);
+  let s = Sexpr.of_seq (Sexpr.lex (Sequence.String.to_seq sexpr)) in
+  Format.printf "parse @[<h>%s@] into @[<h>%a@]@." sexpr (Sexpr.pp_sexpr ~indent:false) s;
   ()
